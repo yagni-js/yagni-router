73aeeda5bd60e3048c5f2dff4c00487ddbe72128
Yuri Egorov <ysegorov@gmail.com>
2018-03-01 20:22:39 +0300

    + initial rollup and eslint configurations

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+
+import pkg from './package.json';
+import eslint from 'rollup-plugin-eslint';
+
+export default [
+  {
+    input: 'src/main.js',
+    output: [
+      {file: pkg.main, format: 'cjs'},
+      {file: pkg.module, format: 'es'}
+    ],
+    external: [
+      '@yagni-js/yagni'
+    ],
+    plugins: [
+      eslint({throwOnError: true})
+    ]
+  }
+];
